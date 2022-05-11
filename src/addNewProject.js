7e0d1fe3055ba8bd111b0d4f8fda7c93a5eee1a1
zoro <jagbetuyi001@gmail.com>
2022-05-11 22:26:09 +0100

    add project functionality

--- src/addNewProject.js
+++ src/addNewProject.js
@@ -1,18 +1,31 @@
+const newProject = document.querySelector('.addNewProject')
+const taskForm = document.querySelector('.form')
+const projectForm = document.querySelector('.project-name')
+const projectName = document.querySelector('#project-name')
+const button = document.querySelector('.project-button')
+const navbar = document.querySelector('.navbar')
+
+const newProjectItem = () =>{
+    let newItem = document.createElement('li')
+    newItem.textContent = projectName.value
+    newItem.classList.add('createdLi')
+    navbar.appendChild(newItem)
+}
 
 
 const addNewProject = () => {
-    
-    const newProject = document.querySelector('.addNewProject')
-    const taskForm = document.querySelector('.form')
-    const projectForm = document.querySelector('.project-name')
 
     const addProject = () => {
-
         projectForm.classList.toggle('show')
-        console.log('clicked')
     }
-
     newProject.addEventListener('click', addProject)
+
+    const registerButton = () => {
+        newProjectItem()
+        projectName.textContent = ''
+        
+    }
+    button.addEventListener('click', registerButton)
 }
 
 
