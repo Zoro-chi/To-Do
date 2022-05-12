04e0f39fd819065d5aa1bbeab3df59bb512a3033
zoro <jagbetuyi001@gmail.com>
2022-05-12 02:34:36 +0100

    adding tasks

--- src/addNewProject.js
+++ src/addNewProject.js
@@ -1,20 +1,34 @@
 const newProject = document.querySelector('.addNewProject')
-const taskForm = document.querySelector('.form')
 const projectForm = document.querySelector('.project-name')
 const projectName = document.querySelector('#project-name')
 const button = document.querySelector('.project-button')
 const navbar = document.querySelector('.navbar')
 
 const newProjectItem = () =>{
-    let newItem = document.createElement('li')
-    newItem.textContent = projectName.value
+    const newItem = document.createElement('li')
+    const dltIcon = document.createElement('img')
+    dltIcon.src = "../src/images/addIcon.png" 
+    dltIcon.classList.add('dltIcon')
+    newItem.textContent = `${projectName.value}`
     newItem.classList.add('createdLi')
+    newItem.append(dltIcon)
     navbar.appendChild(newItem)
+    
+    const dltProject = () =>{
+        navbar.removeChild(newItem)
+    }
+    dltIcon.addEventListener('click', dltProject)
 }
 
+class Projects{
+    constructor(name){
+        this.name = name
+    }
+}
+
+const projectLibrary = []
 
 const addNewProject = () => {
-
     const addProject = () => {
         projectForm.classList.toggle('show')
     }
@@ -22,7 +36,10 @@ const addNewProject = () => {
 
     const registerButton = () => {
         newProjectItem()
+        const newProjectObject = new Projects(projectName.value)
+        projectLibrary.push(newProjectObject)
         projectName.textContent = ''
+        projectForm.classList.toggle('show')
         
     }
     button.addEventListener('click', registerButton)
@@ -30,4 +47,4 @@ const addNewProject = () => {
 
 
 
-export {addNewProject}
+export {addNewProject, projectLibrary,}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import {addNewProject} from "./addNewProject.js"
+import {addNewProject, projectLibrary} from "./addNewProject.js"
+
 
 addNewProject()
+
+const createdLi = document.querySelector('.createdLi')
+const taskForm = document.querySelector('.form')
+
+const show = () =>{
+    taskForm.classList.toggle('show')
+}
+createdLi.addEventListener('click', show)
