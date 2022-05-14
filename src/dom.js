7b207674f8f80b7908b0024f9bdef28409336ab4
zoro <jagbetuyi001@gmail.com>
2022-05-14 01:17:29 +0100

    dltButton added

---
+++ src/dom.js
@@ -0,0 +1,12 @@
+// SELECTORS
+const newProject = document.querySelector('.addNewProject')
+const projectForm = document.querySelector('.project-name')
+const projectName = document.querySelector('#project-name')
+const button = document.querySelector('.project-button')
+const navbar = document.querySelector('.navbar')
+
+
+export {newProject, projectForm, projectName, button, navbar}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
-import {addNewProject, projectLibrary} from "./addNewProject.js"
+import {newProject, projectForm, projectName, button, navbar} from './dom.js'
 
-
-addNewProject()
-
-const createdLi = document.querySelector('.createdLi')
-const taskForm = document.querySelector('.form')
-
-const show = () =>{
-    taskForm.classList.toggle('show')
+class Projects{
+    constructor(name){
+        this.name = name
+        this.tasks = []
+    }
+    addTask(task){
+        this.tasks.push(task)
+    }
+    removeTask(task){
+        
+    }
 }
-createdLi.addEventListener('click', show)
+
+let projectLibrary = []
+
+// Dynamically created li's = new ToDo project
+const newProjectItem = () =>{
+    const createdLi = document.createElement('li')
+    const newItem = document.createElement('div')
+    newItem.classList.add('newItemDiv')
+    const dltButton = document.createElement('button')
+    dltButton.classList.add('dltButton')
+    const dltIcon = document.createElement('i')
+    dltIcon.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    dltIcon.classList.add('dltIcon')
+    dltButton.appendChild(dltIcon)
+    createdLi.textContent = `${projectName.value}`
+    createdLi.classList.add('createdLi')
+    newItem.appendChild(createdLi)
+    newItem.appendChild(dltButton)
+    navbar.appendChild(newItem)
+    
+    const dltProject = () =>{
+        // REMOVES PROJECT ITEM FROM NAVBAR
+        navbar.removeChild(newItem)
+        // let indexOfItem = 
+    }
+    dltButton.addEventListener('click', dltProject)
+    return {newItem}
+}
+
+const addProject = () => {
+    projectForm.classList.toggle('show')
+}
+newProject.addEventListener('click', addProject)
+
+const registerButton = () => {
+    projectName.classList.remove('show')
+    newProjectItem()
+    const newProjectObject = new Projects(projectName.value)
+    projectLibrary.push(newProjectObject)
+    projectName.innerHTML = ''
+    projectForm.classList.toggle('show')
+    console.log(projectLibrary)
+}
+button.addEventListener('click', registerButton)
+
+
+
+
