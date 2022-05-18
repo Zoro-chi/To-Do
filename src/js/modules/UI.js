c8d72f1b9fda7201c85cfe69aae741dd5c436013
zoro <jagbetuyi001@gmail.com>
2022-05-19 00:16:23 +0100

    UI

--- src/js/modules/UI.js
+++ src/js/modules/UI.js
@@ -4,6 +4,7 @@ import {Project, projectLibrary} from './project.js'
 const projectNameDiv = document.querySelector('.project-name')
 const addNewProject = document.querySelector('.addNewProject')
 const display = document.querySelector('.display')
+const registerButton = document.querySelector('.project-button')
 
 class UI{
 
@@ -12,66 +13,45 @@ class UI{
     static addProject = () =>{
         // SELECT PROJECT FORM INPUT & ADD 'SHOW' CLASS (VISIBILITY)
         projectNameDiv.classList.toggle('show')
-
-        // CREATE NEW PROJECT AND ADD TO PROJECT LIBRARY AND UI
-        const createProject = () =>{
-            // HIDE INPUT FORM
-            projectNameDiv.classList.toggle('show')
-
-            const newProjectName = document.querySelector('#project-name').value
-            const newProjectObject = new Project(newProjectName)
-            projectLibrary.push(newProjectObject)
-            console.log(newProjectName)
-            console.log(projectLibrary)
-
-            const newProject = document.createElement('li')
-            newProject.classList.add('createdLi')
-            newProject.innerText = newProjectName
-
-            // ADD A "ADD TASK BUTTON" AND "DELETE PROJECT BUTTON"
-            newProject.innerHTML = `${newProjectName} 
-            <button class='addTaskButton'> <i class='fa-solid fa-plus add-task-image'></i> </button> 
-            <button class='dltButton'> <i class='fa-solid fa-trash dltIcon'></i> </button> `
-            document.querySelector('.addTaskButton').addEventListener('click', () =>{
-                const taskFormDiv = document.createElement('div')
-                taskFormDiv.classList.add('task-form-div')
-                taskFormDiv.innerHTML = `
-                <form class='task-form'>
-                    <div class='task-name'>
-                        <label for='task-name'> Name </label>
-                        <input id='task-name' type='text' placeholder='Name'>
-                    </div>
-
-                    <div class='task-description'>
-                        <label for='task-description'> Description </label>
-                        <input id='task-name' type='textarea' placeholder='Description' rows=10>
-                    </div>
-
-                    <div class='due-date'>
-                        <label for='due-date'> Due date </label>
-                        <input id-'due-date' type='date'>
-                    </div>
-
-                    <div class='task-form-buttons'>
-                    <button class='addTask'> <i class='fa-solid fa-plus add-task-image'></i> </button>
-                    <button class='dltButton'> <i class='fa-solid fa-trash dltIcon'></i> </button>
-                    </div>
-                </form>
-                `
-                display.appendChild(taskFormDiv)
-            })
-            document.querySelector('.dltButton').addEventListener('click', dltProject)
-
-            const projectDisplay = document.querySelector('.navbar')
-            projectDisplay.appendChild(newProject)
-        }
-        document.querySelector('.project-button').addEventListener('click', createProject)
-
     }
+    // CREATE NEW PROJECT AND ADD TO PROJECT LIBRARY AND UI
+    static createProject = () =>{
+        // HIDE INPUT FORM
+        projectNameDiv.classList.toggle('show')
+
+        const newProjectName = document.querySelector('#project-name').value
+        const newProjectObject = new Project(newProjectName)
+        projectLibrary.push(newProjectObject)
+        console.log(newProjectName)
+        console.log(projectLibrary)
+
+        const newProjectDiv = document.createElement('div')
+        newProjectDiv.classList.add('newProjectDiv')
+        const newProject = document.createElement('li')
+        newProject.classList.add('createdLi')
+        newProject.innerText = newProjectName
+        newProjectDiv.appendChild(newProject)
+        const deleteProjectButton = document.createElement('button')
+        deleteProjectButton.classList.add('dltButton')
+        const deleteProjectIcon = document.createElement('i')
+            
+        newProject.append(deleteProjectButton)
+        const addTaskToProject = document.createElement('button')
+        addTaskToProject.classList.add('addTasm')
+        newProject.append(addTaskToProject)
+
+
+        const projectDisplay = document.querySelector('.navbar')
+        projectDisplay.appendChild(newProjectDiv)
+    }
+    
 }
 
 
 // EVENT LISTENERS
 addNewProject.addEventListener('click', UI.addProject)
+registerButton.addEventListener('click', UI.createProject)
+
+
 
 export {UI}
